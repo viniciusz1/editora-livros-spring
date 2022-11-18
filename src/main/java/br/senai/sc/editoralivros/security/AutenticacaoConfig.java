db636e0e9e77a92016c70830e432e12a7335cd80
viniciusz1 <vinicius101313@gmail.com>
2022-11-18 16:59:33 -0300

    ok

--- src/main/java/br/senai/sc/editoralivros/security/AutenticacaoConfig.java
+++ src/main/java/br/senai/sc/editoralivros/security/AutenticacaoConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -30,7 +31,8 @@ public class AutenticacaoConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.POST, "/login").permitAll()
                 .anyRequest().authenticated()
                 .and().csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().addFilterBefore(new AutenticacaoFiltro(autenticacaoService), UsernamePasswordAuthenticationFilter.class);
     }
 
     //Configura a autenticação
